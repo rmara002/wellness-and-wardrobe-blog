39e1367d4dd68361e3172d2a47db9b7c730e006e
rmara002 <rmara002@doc.gold.ac.uk>
2023-10-28 22:16:47 +0100

    Changed the username and email to be unique

--- create_db.sql
+++ create_db.sql
@@ -5,11 +5,11 @@ USE myBlog;
 -- Users table
 CREATE TABLE users (
     id INT AUTO_INCREMENT,
-    username VARCHAR(50),
+    username VARCHAR(50) UNIQUE,
     password hashedPassword VARCHAR(255);
     first_name VARCHAR(255);
     last_name VARCHAR(255);
-    email VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
     bio TEXT,
     profile_picture VARCHAR(255),
     PRIMARY KEY(id)
